55866bed24f94498756582e7175c73cd8773fb29
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-07-31 14:38:55 +0300

    installed playwright

---
+++ PlaywrightTests/tests/example.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+  await page.goto('https://localhost:44336');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle('Gym Management - Home');
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://localhost:44336');
+
+  // Click the get started link.
+  await page.getByRole('link', { name: 'Join Now ↓' }).click();
+
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole('heading', { name: 'Push Your Limits' })).toBeVisible();
+});
