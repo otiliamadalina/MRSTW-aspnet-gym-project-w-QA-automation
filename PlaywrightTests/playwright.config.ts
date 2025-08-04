8d599560553a9663705fe0dc3300c28856d3448d
IrinaDimitriu <irina.dimitriu@ovo.com>
2025-08-04 09:22:55 +0300

    changed port

--- PlaywrightTests/playwright.config.ts
+++ PlaywrightTests/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://localhost:5000",
+    baseURL: "http://localhost:44336",
     trace: "on-first-retry",
     ignoreHTTPSErrors: true,
   },
