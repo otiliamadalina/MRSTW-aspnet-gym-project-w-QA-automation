7967c1f7b16487bc7f097e030edf7bfc95704f3a
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-08-05 22:24:02 +0300

    resolved issues with index file

--- PlaywrightTests/actions/index.ts
+++ PlaywrightTests/actions/index.ts
@@ -4,10 +4,7 @@ import NavigationActions from "./navigation.actions";
 import HomeActions from "./home.actions";
 import CommonActions from "./common.actions";
 import navbarFooterActions from "./navbarFooter.actions";
-<<<<<<< HEAD
 import AboutActions from "./about.action";
-=======
->>>>>>> 2-test-navigation-bar-and-footer-for-all-pages
 
 export default class App {
   base: BaseActions;
@@ -15,10 +12,7 @@ export default class App {
   common: CommonActions;
   home: HomeActions;
   navbarFooter: navbarFooterActions;
-<<<<<<< HEAD
   about: AboutActions;
-=======
->>>>>>> 2-test-navigation-bar-and-footer-for-all-pages
 
   constructor(page: Page, context: BrowserContext) {
     this.base = new BaseActions(page, context);
@@ -26,9 +20,6 @@ export default class App {
     this.common = new CommonActions(page, context);
     this.home = new HomeActions(page, context);
     this.navbarFooter = new navbarFooterActions(page, context);
-<<<<<<< HEAD
     this.about = new AboutActions(page, context);
-=======
->>>>>>> 2-test-navigation-bar-and-footer-for-all-pages
 }
 }
