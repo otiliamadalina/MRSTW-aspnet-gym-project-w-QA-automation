9d6fb103acedfb4b57a918938f85b786fdec2f77
otiliamadalina <otilia-madalina.leporda@isa.utm.md>
2025-08-26 12:17:30 +0300

    added verifying templates

--- PlaywrightTests/actions/membership.actions.ts
+++ PlaywrightTests/actions/membership.actions.ts
@@ -2,7 +2,7 @@ import BaseActions from "./base.actions";
 import HomePage from "../pages/home.page";
 import routes from "../resources/routes.json";
 import strings from "../resources/strings.json";
-import { BrowserContext, expect, Page } from "@playwright/test";
+import { BrowserContext, expect, Locator, Page } from "@playwright/test";
 import CommonActions from "./common.actions";
 import MembershipPage from "../pages/membership.page";
 
@@ -14,8 +14,36 @@ export default class MembershipActions extends CommonActions {
     this.membership = new MembershipPage(page, context);
   }
 
-  async locateMembershipCards(){
+  async locateMembershipCards() {
     await expect(this.membership.membershipCards).toBeVisible();
   }
 
+  async verifyMembershipHeader(
+    membershipName: string,
+    membershipLocator: Locator
+  ) {}
+
+  async checkMembershipHeaders() {}
+
+  async verifyMembershipDetails(
+    membershipDetails: string,
+    membershipLocator: Locator
+  ) {}
+
+  async checkMembershipDetails() {}
+
+  async verifyMembershipPrice(
+    membershipPrice: string,
+    membershipLocator: Locator
+  ) {}
+
+  async checkMembershipPrice() {}
+
+  async verifyMembershipButton(membershipButton: string, button: Locator) {}
+
+  async checkMembershipButtons() {}
+
+  async verifyMembershipBenefitsHeader() {}
+
+  async verifyMembershipsParagraphs() {}
 }
